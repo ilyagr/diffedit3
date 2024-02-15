9995ab0756a2a10bde742fe50da12bf645e96126
Ilya Grigoriev <ilyagr@users.noreply.github.com>
2024-02-14 18:31:07 -0800

    First route

--- backend-warp/src/main.rs
+++ backend-warp/src/main.rs
@@ -11,7 +11,12 @@ struct StaticFiles;
 #[tokio::main]
 async fn main() {
     let static_files = warp_embed::embed(&StaticFiles {});
-    let server = static_files.with(warp::log("http"));
+    let server = warp::get().and(
+        static_files.with(warp::log("http")).or(warp::path("api")
+            .and(warp::path("test.json"))
+            .and(warp::path::end())
+            .map(|| "Test\n")),
+    );
     let listen_to = "127.0.0.1:8080";
     eprintln!("Trying to listen at http://{listen_to}...");
     let listen = listen_to.to_string().to_socket_addrs().unwrap();
