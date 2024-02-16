df33b9d35b3b4a40bedcbc294fa900671eadc772
Ilya Grigoriev <ilyagr@users.noreply.github.com>
2024-02-15 20:13:25 -0800

    Example of a more generic warp processing

--- backend-warp/src/main.rs
+++ backend-warp/src/main.rs
@@ -1,6 +1,8 @@
 use std::net::ToSocketAddrs;
 
 use log::info;
+use serde::Serialize;
+use warp::http::StatusCode;
 use warp::Filter;
 #[derive(rust_embed::RustEmbed)]
 #[folder = "../webapp/dist"]
@@ -8,16 +10,24 @@ struct StaticFiles;
 
 // See also https://github.com/informationsea/warp-embed/blob/master/examples/test-embedded.rs
 
+fn result_to_warp_reply<T: Serialize, Err: Serialize>(result: Result<T, Err>) -> impl warp::Reply {
+    match result {
+        Ok(data) => warp::reply::with_status(warp::reply::json(&data), StatusCode::OK),
+        Err(err) => {
+            warp::reply::with_status(warp::reply::json(&err), StatusCode::INTERNAL_SERVER_ERROR)
+        }
+    }
+}
+
 #[tokio::main]
 async fn main() {
     let static_files = warp_embed::embed(&StaticFiles {});
-    // TODO: Handle rejection
-    // https://github.com/seanmonstar/warp/blob/master/examples/rejections.rs
     let server = warp::get().and(
         static_files.with(warp::log("http")).or(warp::path("api")
             .and(warp::path("get_merge_data"))
             .and(warp::path::end())
-            .map(|| warp::reply::json(&diff_tool_logic::Input::FakeData.scan().unwrap()))),
+            .map(|| diff_tool_logic::Input::FakeData.scan())
+            .map(result_to_warp_reply)),
     );
     let listen_to = "127.0.0.1:8080";
     eprintln!("Trying to listen at http://{listen_to}...");
