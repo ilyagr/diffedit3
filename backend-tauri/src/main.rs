7a87ca7498088fd59164b1d818343ebb6dd04ec8
Ilya Grigoriev <ilyagr@users.noreply.github.com>
2024-02-17 23:02:42 -0800

    Windows -- make this a console app

--- backend-tauri/src/main.rs
+++ backend-tauri/src/main.rs
@@ -1,5 +1,7 @@
-// Prevents additional console window on Windows in release, DO NOT REMOVE!!
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+// Would prevent additional console window on Windows in release. We want the
+// console window and waiting to exit until completion, though.
+
+// #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 use clap::Parser;
 use indexmap::IndexMap;
