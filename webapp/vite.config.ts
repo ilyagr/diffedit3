84d3555915dd6a7008526f37b5d9ab0915070196
Ilya Grigoriev <ilyagr@users.noreply.github.com>
2024-02-20 21:05:12 -0800

    webapp & vite: Trying workarounds for non-determenistic Vite build
    https://github.com/vitejs/vite/discussions/15994#discussion-6247191

--- webapp/vite.config.ts
+++ webapp/vite.config.ts
@@ -31,7 +31,17 @@ export default defineConfig({
     }),
   ],
   build: {
+    commonjsOptions: {
+      // https://github.com/vitejs/vite/issues/13672#issuecomment-1784110536
+      // It *seems* that this workaround makes a difference. I'm
+      // *guessing* this is related to CodeMirror5 being CommonJS and
+      // possibly having some crazy recursive includes.
+      strictRequires: true
+    },
     rollupOptions: {
+      // https://github.com/vitejs/vite/issues/10506#issuecomment-1367718113
+      // TODO: It's possible that this workaround makes no difference
+      maxParallelFileOps: 1,
       plugins: [
         // externalGlobals() would be nice, but doesn't seem to work
         // externalGlobals({ diff_match_patch: "diff_match_patch" }),
